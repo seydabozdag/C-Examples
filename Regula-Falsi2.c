10878002c894894fb5154189ca2345597764709e
Şeyda Bozdağ <149304443+seydabozdag@users.noreply.github.com>
2024-11-13 23:41:42 +0300

    Regula-Falsi Örnekleri

---
+++ Regula-Falsi2.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+#include<math.h>
+
+double F(double x) {
+  return pow(x, 3) - 5 * pow(x, 2) - 2 * x + 10;
+}
+
+int main() {
+  double a = 1, b = 3, x, x0, x1, epsilon = 0.1;
+  int i = 0;
+
+  if(F(a) * F(b) >= 0) {
+    printf("%.4lf ve %.4lf araliginda kok yoktur  ", a, b);
+    return 0;
+  }
+
+  x0 = (a * F(b) - b * F(a)) / (F(b) - F(a));
+
+  do {
+    i++;
+    printf("%d. adimda yaklasik kok: %.4lf\n", i, x0);
+    x1 = x0;
+
+    if(F(x0) * F(a) < 0) {
+      b = x0;
+    } else {
+      a = x0;
+    }
+
+    printf("Yeni aralik: [%.4lf, %.4lf]\n", a, b);
+
+    x0 = (a * F(b) - b * F(a)) / (F(b) - F(a));
+  } while(fabs(x0 - x1) > epsilon);
+
+  printf("Yaklasik kok %d. adimda hesaplandi.\n", ++i);
+  printf("Yaklasik kok: %.4lf\n", x0);
+  printf("F(%.4lf) = %.4lf\n", x0, F(x0));
+
+  return 0;
+
+}
